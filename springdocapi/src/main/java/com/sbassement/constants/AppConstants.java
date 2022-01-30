d3420eb075c892eea795e890ecd07b467712e87b
prasadpr1 <prasad_pr1@yahoo.com>
2022-01-30 19:20:25 +0530

    welcome

--- springdocapi/src/main/java/com/sbassement/constants/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.sbassement.constants;
-
-public interface AppConstants {
-	
-	 String EMPLOYEE_BASE_PATH = "/api/employee/management/";
-	 
-	 String PRODUCT_BASE_PATH = "/product";
-
-}
