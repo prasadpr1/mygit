70cf1e5fa7342b78faa46bdaf9922559a6a5c792
prasadpr1 <prasad_pr1@yahoo.com>
2022-10-23 18:53:45 +0530

    Interview questions part1
    Interview questions

---
+++ InterviewQuestions/src/com/helloworld/InterviewQuestion1.java
@@ -0,0 +1,20 @@
+package com.helloworld;
+
+public class InterviewQuestion1 {
+public static void main(String[] args) {
+	
+	
+	String s1 = "Hello-World";
+	String s2 = "Hello—World";
+	String s3 = new String("Hello—World");
+	
+	System.out.println(s1==s2);
+	System.out.println(s2==s3);
+	System.out.println(s3==s1);
+	System.out.println(s1.equals(s2));
+	System.out.println(s2.equals(s3));
+	System.out.println(s2.intern().equals(s3));
+	
+			
+}
+}
